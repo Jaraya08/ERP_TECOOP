45b6005a8296f5ba44f3abaaf2f726a1415ce507
Jordan Hanan Araya Leiva <jaraya08@estudiantec.cr>
2020-01-20 23:50:17 -0600

    .

---
+++ ERP_TECOOP/Server/SesionControllers/SesionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_TECOOP.Server.SesionControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SesionController : ControllerBase
+    {
+        // GET: api/Sesion
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Sesion/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Sesion
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Sesion/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ERP_TECOOP/Shared/Sesion/Usuario_e.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_TECOOP.Shared.Sesion
+{
+    public class Usuario_e
+    {
+        public string idUsuario { get; set; }
+
+        public string nombreUsuario { get; set; }
+
+        public string contrasenha { get; set; }
+
+    }
+}
